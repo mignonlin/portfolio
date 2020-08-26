fdfd594fbd8b7ca48fac716f621bcbb7cb326f8e
전미뇽 <totheworldjmk@gmail.com>
2020-08-26 23:43:04 +0900

    1. progressbar 가운데로 가게 만듬 + title 적어줌

--- kr/js/progressbar.js
+++ kr/js/progressbar.js
@@ -7,55 +7,58 @@
  * - listening to `circle-animation-progress` event and display the animation progress: from 0 to 100%
  */
 $(document).ready(function () {
-  $(".second.circle")
-    .circleProgress({
-      value: 0.8,
-    })
-    .on("circle-animation-progress", function (event, progress) {
-      $(this)
-        .find("strong")
-        .html(Math.round(80 * progress) + "<i>%</i>");
-    });
 
-  $(".second2.circle")
-    .circleProgress({
-      value: 0.85,
-    })
-    .on("circle-animation-progress", function (event, progress) {
-      $(this)
-        .find("strong")
-        .html(Math.round(85 * progress) + "<i>%</i>");
-    });
+  $("#section2").hover(function () {
+    $(".second.circle")
+      .circleProgress({
+        value: 0.8,
+      })
+      .on("circle-animation-progress", function (event, progress) {
+        $(this)
+          .find("strong")
+          .html(Math.round(80 * progress) + "<i>%</i>");
+      });
 
-  $(".second3.circle")
-    .circleProgress({
-      value: 0.75,
-    })
-    .on("circle-animation-progress", function (event, progress) {
-      $(this)
-        .find("strong")
-        .html(Math.round(75 * progress) + "<i>%</i>");
-    });
+    $(".second2.circle")
+      .circleProgress({
+        value: 0.85,
+      })
+      .on("circle-animation-progress", function (event, progress) {
+        $(this)
+          .find("strong")
+          .html(Math.round(85 * progress) + "<i>%</i>");
+      });
 
-  $(".second4.circle")
-    .circleProgress({
-      value: 0.8,
-    })
-    .on("circle-animation-progress", function (event, progress) {
-      $(this)
-        .find("strong")
-        .html(Math.round(80 * progress) + "<i>%</i>");
-    });
+    $(".second3.circle")
+      .circleProgress({
+        value: 0.75,
+      })
+      .on("circle-animation-progress", function (event, progress) {
+        $(this)
+          .find("strong")
+          .html(Math.round(75 * progress) + "<i>%</i>");
+      });
 
-  $(".second5.circle")
-    .circleProgress({
-      value: 0.7,
-    })
-    .on("circle-animation-progress", function (event, progress) {
-      $(this)
-        .find("strong")
-        .html(Math.round(70 * progress) + "<i>%</i>");
-    });
+    $(".second4.circle")
+      .circleProgress({
+        value: 0.8,
+      })
+      .on("circle-animation-progress", function (event, progress) {
+        $(this)
+          .find("strong")
+          .html(Math.round(80 * progress) + "<i>%</i>");
+      });
+
+    $(".second5.circle")
+      .circleProgress({
+        value: 0.7,
+      })
+      .on("circle-animation-progress", function (event, progress) {
+        $(this)
+          .find("strong")
+          .html(Math.round(70 * progress) + "<i>%</i>");
+      });
+  });
 
 });
 //this is a test! 
